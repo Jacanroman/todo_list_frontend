b880073a3e7bd1b1fc931d9c55df47509aa817da
Jacanroman <jacanroman@gmail.com>
2020-03-30 12:11:08 +0100

    changes on Task and TaskDone

--- src/Task/Task.js
+++ src/Task/Task.js
@@ -26,35 +26,23 @@ function Task(props) {
     return (
 
         <div className="row align-items-center mt-2 mb-2">
-
-            {props.completed === false &&
-                <div className="col-5">
-                    <p className="m-0 p-1 task">{props.text}</p>
-                </div>
-            }
-            {props.completed === false &&
-                <div className="col-3">
-                    <p className="m-0 p-1 dueDate">{moment(props.dueDate).format('Do MMM YYYY')}</p>
-                </div>
-            }
-            <div className="col-2 p-0">
-                {props.completed === false &&
-                    <i className="fas fa-check-circle fa-lg d-block d-md-none"></i>
-                }
-                {props.completed === false &&
-                    <input type="button" className="btn btn-success btn-sm d-none d-md-block" onClick={handleCompleteClick} value="MARK DONE" />
-                }
+            <div className="col-5">
+                <p className="m-0 p-1 task">{props.text}</p>
             </div>
-            <div className="col-2 p-0">
-                {props.completed === false &&
-                    <i className="fas fa-trash-alt fa-lg d-block d-md-none"></i>
-                }
-                
-                {props.completed === false &&
-                    <input type="button" className="btn btn-danger btn-sm d-none d-md-block" onClick = {handleDeleteClick} value="DELETE" />
-                }
+            
+            <div className="col-3">
+                <p className="m-0 p-1 dueDate">{moment(props.dueDate).format('Do MMM YYYY')}</p>
             </div>
+            
+            <div className="col-2 p-0">
+                <i className="fas fa-check-circle fa-lg d-block d-md-none"></i>
+                <input type="button" className="btn btn-success btn-sm d-none d-md-block" onClick={handleCompleteClick} value="MARK DONE" />
+             </div>
 
+            <div className="col-2 p-0">
+                <i className="fas fa-trash-alt fa-lg d-block d-md-none"></i>
+                <input type="button" className="btn btn-danger btn-sm d-none d-md-block" onClick = {handleDeleteClick} value="DELETE" />
+            </div>
 
         </div>
 
--- src/TaskDone/TaskDone.js
+++ src/TaskDone/TaskDone.js
@@ -13,24 +13,17 @@ function TaskDone(props){
     return(
         
             <div className="row align-items-center mt-2 mb-2">
-                
-                {props.completed === true && 
                 <div className="col-7">
                     <p className="m-0 p-1 task task-done">{props.text}</p>
                 </div>
-                }
-                {props.completed === true &&
+                
                 <div className="col-3">
                     <p className="m-0 p-1 dueDate">{moment(props.dueDate).format('Do MMM YYYY')}</p>
                 </div>
-                }
+                
                 <div className="col-2 p-0">
-                {props.completed === true &&
                     <i className="fas fa-trash-alt fa-lg d-block d-md-none"></i>
-                }
-                {props.completed === true &&
                     <input type="button" className="btn btn-danger btn-sm d-none d-md-block" onClick = {handleDeleteClick} value="DELETE"/>
-                }
                 </div>
             </div>
         
