44c48392c2729a8a7c23723a2b424770ea5dd8c3
Jacanroman <jacanroman@gmail.com>
2020-04-02 11:40:17 +0100

    Updates

---
+++ precache-manifest.6b4d147e5b356c18327d2827a7c11a73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "443612f81de54049880d3140a76bd7eb",
+    "url": "/todo_list_frontend/index.html"
+  },
+  {
+    "revision": "246ce46edbacc1719de6",
+    "url": "/todo_list_frontend/static/css/main.634d1bde.chunk.css"
+  },
+  {
+    "revision": "aab3e902c9186545c1e9",
+    "url": "/todo_list_frontend/static/js/2.0200c6dd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/todo_list_frontend/static/js/2.0200c6dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "246ce46edbacc1719de6",
+    "url": "/todo_list_frontend/static/js/main.2ee3414d.chunk.js"
+  },
+  {
+    "revision": "a1ca9be16980ffd05b88",
+    "url": "/todo_list_frontend/static/js/runtime-main.858c6f9e.js"
+  },
+  {
+    "revision": "6e56b1e114cafef8e57b04009369e42e",
+    "url": "/todo_list_frontend/static/media/28518.6e56b1e1.jpg"
+  }
+]);
